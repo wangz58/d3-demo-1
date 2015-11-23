d0890e7a7e1c7beaaf6b417ff47a544ad7317254
Richard Ding <rmding95@uw.edu>
2015-11-23 02:44:10 -0800

    comments done

--- supacool.js
+++ supacool.js
@@ -1,60 +1,57 @@
 'use strict';
 
 var makeChart = function(data) {
-    var ratings = [];
-    var movies = [];
+    var ratings = []; //stores the ratings
+    var movies = []; //stores the titles of each movie
     for (var i = 0; i < data.length; i++) {
         ratings.push(data[i].imdbRating);
         movies.push(data[i].Title);
     }
 
-    var width = 900,
+    //defining the width and height of our graph
+    var width = 900, 
         height = 450;
 
-    var y = d3.scale.linear()
-        .range([height, 0])
-        .domain([0, d3.max(ratings)]);
+    var y = d3.scale.linear() //creates a linear scale for the y variable
+        .range([height, 0]) //the set of acceptable output
+        .domain([0, d3.max(ratings)]); //the set of acceptable input values
 
-    var chart = d3.select(".chart")
+    var chart = d3.select(".chart") //creating the chart and setting its length and width
         .attr("width", width)
         .attr("height", height);
 
-    var barWidth = width / ratings.length;
-    
-    // var tip = d3.tip()
-    //   .attr('class', 'd3-tip')
-    //   .offset([-10, 0])
-    //   .html(function(d) { return d; })
+    var barWidth = width / ratings.length; //setting the width of each bar in the bar graph
 
-    // chart.call(tip);
+    //adding our data to the bar graph
+    var bar = chart.selectAll("g.transform") 
+        .data(ratings) //passing in our ratings array
+        .enter().append("g") //creating a new g element for every data point
+        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; }) //positioning the bars correctly
 
-    var bar = chart.selectAll("g.transform")
-        .data(ratings)
-        .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; })
-        // .on('mouseover', tip.show)
-        // .on('mouseout', tip.hide);
 
+    //each of these rect elements will represent a data point
     bar.append("rect")
-        .attr("y", function(d) { return y(d); })
-        .attr("height", function(d) { return height - y(d); })
-        .attr("width", barWidth - 1)
-        .attr("fill", function(d) {
+        .attr("y", function(d) { return y(d); }) //setting the y-value of each rect to the corresponding rating value
+        .attr("height", function(d) { return height - y(d); }) //setting the height of each rect
+        .attr("width", barWidth - 1) 
+        .attr("fill", function(d) { //simple formula that sets the fill of each bar to be redder the higher the rating is
             return "rgb( " + (d*20) + " , 0, 0)"
         });
 
+    //adding the labels to each bar 
     bar.append("text")
-        .attr("x", barWidth / 2)  
-        .attr("y", function(d) { return y(d) + 3; })
+        .attr("x", barWidth / 2)  //making the width of each label half the width of each bar
+        .attr("y", function(d) { return y(d) + 3; }) //adding the label near the top of each bar
         .attr("dy", "1em")
-        .text(function(d) { return d; });
+        .text(function(d) { return d; }); //setting the text of the label to the rating
 
+    //adding the movie title label near the bottom of each bar
     bar.append("text")
-        .data(movies)
+        .data(movies) //passing in our movies array which contains the title of each movie
         .attr("x", barWidth/2)
-        .attr("y", height-30)
+        .attr("y", height-30) //adding the label near the bottom
         .text(function(d) { return d; })
-        .style("font-size", function(d) {
+        .style("font-size", function(d) { //if/else to shrink the font-size if the title is too long
             if (d.length > 10) {
                 return "8px"
             } else {
