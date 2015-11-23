d90f19490f7b8bf22ea665e3e626d2831c8ba65e
Richard Ding <rmding95@uw.edu>
2015-11-22 21:14:55 -0800

    axis lables not showing

--- supacool.js
+++ supacool.js
@@ -11,22 +11,37 @@ var makeChart = function(data) {
     var width = 960,
         height = 500;
 
+    var x = d3.scale.ordinal()
+        .domain(movies)
+        .rangePoints([0, width]);
+
     var y = d3.scale.linear()
         .range([height, 0])
         .domain([0, d3.max(ratings)]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
     var chart = d3.select(".chart")
         .attr("width", width)
         .attr("height", height);
 
     var barWidth = width / ratings.length;
+    //x.domain(data.map(function(d) { return d.Title; }));
 
-    var bar = chart.selectAll("g")
+    var bar = chart.selectAll("g.transform")
         .data(ratings)
         .enter().append("g")
         .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
     bar.append("rect")
+        //.attr("x", function(d) { return x(d); })
         .attr("y", function(d) { return y(d); })
         .attr("height", function(d) { return height - y(d); })
         .attr("width", barWidth - 1);
@@ -36,6 +51,9 @@ var makeChart = function(data) {
         .attr("y", function(d) { return y(d) + 3; })
         .attr("dy", ".75em")
         .text(function(d) { return d; });
+
+
+
 
     // var chart = d3.select(".chart")
     //     .selectAll("div")
